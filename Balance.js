f24bce5765b10da10e3f5ac6b28ccef4f97404ea
sudip98k <103334485+sudip98k@users.noreply.github.com>
2023-03-27 19:58:35 +0530

    Add files via upload

---
+++ Balance.js
@@ -0,0 +1,19 @@
+import React,{useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
+
+
+function Balance() {
+  const {transactions}=useContext(GlobalContext);
+  // console.log(context);
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1 id="balance">${total}</h1>
+    </>
+  )
+}
+
+export default Balance
---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h2>
+      My-Expenses
+    </h2>
+  )
+}
+
+export default Header
